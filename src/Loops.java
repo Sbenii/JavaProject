cd2c408f33742cc346fcfadd4c3eec4fe289ce35
Sbenii <sezeranben@gmail.com>
2025-08-26 01:05:51 +0200

    Additional filw

---
+++ src/Loops.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args){
+        Scanner Input=new Scanner(System.in);
+        for (int i=0;i<=100;++i){
+            System.out.println(i);
+        }
+    }
+}
