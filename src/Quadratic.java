833fcb8d6e583c1f26bfdbcd266705b0f077054a
Sbenii <sezeranben@gmail.com>
2025-08-29 21:38:01 +0200

    Classes

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -12,11 +12,22 @@ public class Quadratic {
         System.out.print("Enter the value of c: ");
         c=Input.nextDouble();
         d=(b*b)-(4*a*c);
-        System.out.print("The delta of the equation: ");
+        System.out.println("The delta of the equation: "+d);
         if (d==0){
-            root_1=(-b+Math.sqrt(d))/(2*a);
+            root_1=(-(b)+Math.sqrt(d))/(2*a);
             root_2=root_1;
+            System.out.println("We have a double root!!");
             System.out.println("The first root of the equation: "+root_1);
+            System.out.println("The second root of the equation: "+root_2);
+        } else if (d>0) {
+            root_1=(-(b)+Math.sqrt(d))/(2*a);
+            root_2=(-b-Math.sqrt(d))/(2*a);
+            System.out.println("We have two distinct roots!!");
+            System.out.println("The first root of the equation: "+root_1);
+            System.out.println("The second root of the equation: "+root_2);
+        }
+        else {
+            System.out.println("There are no real roots for the equation!!!!!");
         }
     }
 }
